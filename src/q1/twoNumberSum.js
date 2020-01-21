3f6bf3ad961e76068891e9a5ca7b4e138fce509f
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-01-21 17:58:00 +0100

    ft(add-implementation)
    - solve using O(n) time & space

--- src/q1/twoNumberSum.js
+++ src/q1/twoNumberSum.js
@@ -1,21 +1,38 @@
+// /**
+//  * @desc O(n^2) solution | O(1) space
+//  * @param {array} numArray 
+//  * @param {number} targetSum 
+//  */
+// const twoNumberSum = (numArray, targetSum) => {
+//   for (let i = 0; i < numArray.length - 1; i++) {
+//     let firstNum = numArray[i];
+//     for (let j = i + 1; j < numArray.length; j++) {
+//       let secondNum = numArray[j];
+//       let tempSum = firstNum + secondNum;
+//       if (tempSum === targetSum) {
+//         return [firstNum, secondNum].sort((a,b)=> a-b);
+//       }
+//     }
+//   }
+//   return [];
+// };
+
 /**
- * @desc 
+ * @desc O(n) time | O(n) space
  * @param {array} numArray 
  * @param {number} targetSum 
  */
 const twoNumberSum = (numArray, targetSum) => {
-  for (let i = 0; i < numArray.length - 1; i++) {
-    let firstNum = numArray[i];
-    for (let j = i + 1; j < numArray.length; j++) {
-      let secondNum = numArray[j];
-      let tempSum = firstNum + secondNum;
-      if (tempSum === targetSum) {
-        return [firstNum, secondNum].sort((a,b)=> a-b);
-      }
+  let numHash = {};
+  for (const currentNum of numArray) {
+    let targetNum = targetSum - currentNum;
+    if (targetNum in numHash) {
+      return [currentNum, targetNum].sort((a, b) => a - b);
+    } else {
+      numHash[currentNum] = true;
     }
   }
   return [];
 };
 
-// const twoNumberSum = (numArray, targetSum)=> {}
 module.exports = twoNumberSum;
